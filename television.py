0e59e5bb594ed4fc2394f303acaee7227b686d1f
Seth McGill <sethpmcgill@gmail.com>
2026-04-13 14:27:23 -0500

    Update television.py

--- television.py
+++ television.py
@@ -3,33 +3,34 @@ class Television:
     MAX_VOLUME = 2
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
-    
-    def __init__(self):
+
+    def __init__(self) -> None:
+        """
+        Creates a new TV object with attributes status, muted, both set to False,
+        and volume and channel, set to MIN_VOLUME and MIN_CHANNEL, respectively.
+        """
         self.__status = False #power
         self.__muted = False
-        self.__volume = 0
-        self.__channel = 0
+        self.__volume = self.MIN_VOLUME
+        self.__channel = self.MIN_CHANNEL
 
-    def power(self):
+    def power(self) -> None:
         """
         Toggles TV power.
-        :return: None
         """
         self.__status = not self.__status
 
-    def mute(self):
+    def mute(self) -> None:
         """
-        Toggles TV mute option.
-        :return: None
+        If powered, toggles TV mute option.
         """
         if self.__status:
             self.__muted = not self.__muted
 
-    def channel_up(self):
+    def channel_up(self) -> None:
         """
-        Increments the current channel by 1. Moves to channel
-        0 if incrementing results in a channel above 3.
-        :return: None
+        If powered, increments the current channel by 1. Moves to channel
+        MIN_CHANNEL if incrementing results in a channel above MAX_CHANNEL.
         """
         if self.__status:
             self.__channel += 1
@@ -37,11 +38,10 @@ class Television:
             if self.__channel > self.MAX_CHANNEL:
                 self.__channel = self.MIN_CHANNEL
 
-    def channel_down(self):
+    def channel_down(self) -> None:
         """
-        Decrements the current channel by 1. Moves to channel
-        3 if decrementing results in a channel below 0.
-        :return: None
+        If powered, Decrements the current channel by 1. Moves to channel
+        MAX_CHANNEL if decrementing results in a channel below MIN_CHANNEL.
         """
         if self.__status:
             self.__channel -= 1
@@ -49,10 +49,9 @@ class Television:
             if self.__channel < self.MIN_CHANNEL:
                 self.__channel = self.MAX_CHANNEL
 
-    def volume_up(self):
+    def volume_up(self) -> None:
         """
-        Increases TV volume by 1, up to 2.
-        :return: None
+        If powered, increases TV volume by 1, up to 2. Unmutes TV if muted.
         """
         if self.__status:
             if self.__muted: #unmute TV to allow volume changing.
@@ -61,10 +60,9 @@ class Television:
             if self.__volume < self.MAX_VOLUME:
                 self.__volume += 1
 
-    def volume_down(self):
+    def volume_down(self) -> None:
         """
-        If powered, unmutes TV and decreases volume by 1, down to 0.
-        :return: None
+        If powered, decreases TV volume by 1, down to 0. Unmutes TV if muted.
         """
         if self.__status:
             if self.__muted: #unmute TV to allow volume changing.
@@ -73,7 +71,7 @@ class Television:
             if self.__volume > self.MIN_VOLUME:
                 self.__volume -= 1
 
-    def __str__(self):
+    def __str__(self) -> str:
         """
         Displays TV power, channel, and volume information.
         Displays zero volume if TV is muted.
